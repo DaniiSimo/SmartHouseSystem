5c09052099f61fb9df1af9a5429501c115cbc7a3
Barba10s <92178420+Barba10s@users.noreply.github.com>
2024-08-26 15:29:10 +0300

    add_class_and_methotds_notations fix #SMA-0 (#11)
    Description: добавлены докстроки классам и методам

--- python/app/moduleNeuralNetwork/services/processing_parameter_values/datetime.py
+++ python/app/moduleNeuralNetwork/services/processing_parameter_values/datetime.py
@@ -4,35 +4,35 @@ from python.app.moduleNeuralNetwork.services.processing_parameter_values.datetim
 
 class DateAndTime:
     def extract_and_convert_time(self, text: str) -> float:
-        # Извлечение времени из текста с настройкой на ближайшее будущее
+        """
+        Извлечение даты и время из запроса на ествественном языке
+
+        Args:
+            text (str): запрос на естественном языке
+
+        Returns:
+            time_seconds: значение преданного времени в секундах
+        """
         parsed_time = dateparser.parse(text, languages=['ru'], settings={'PREFER_DATES_FROM': 'future'})
 
         if parsed_time:
-            # Приведение к стандартному типу datetime
             parsed_time = datetime(parsed_time.year, parsed_time.month, parsed_time.day,
                                    parsed_time.hour, parsed_time.minute, parsed_time.second)
 
-            # Текущее время
             now = datetime.now()
 
-            # Преобразование в секунды
             time_seconds = (parsed_time - now).total_seconds()
 
-            # Проверка, если parsed_time в прошлом
             if time_seconds < 0:
 
-               
-                # Проверка месяца и добавление месяца, если необходимо
                 if parsed_time.month < now.month or (parsed_time.month == now.month and parsed_time.day < now.day):
-                    # Увеличиваем месяц на 1
                     next_month = (parsed_time.month % 12) + 1
                     year_increment = parsed_time.month // 12
                     parsed_time = parsed_time.replace(month=next_month, year=parsed_time.year + year_increment)
                 else:
-                    # Добавление недели, если дата все еще в этом месяце, но прошлая
+
                     parsed_time += timedelta(weeks=1)
 
-                # Повторное вычисление времени в секундах
                 time_seconds = (parsed_time - now).total_seconds()
 
             return time_seconds
