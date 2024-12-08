7d16777b19263e8a60f4d8d7ede599aac2d11dce
Danisimo <savenkov-lokin2013@yandex.ru>
2024-12-08 19:52:34 +0300

    voice_recognition fix #SMA-38 Исправлен bash скрипт для запуска считывания информации с микрофона

--- device/python/start.sh
+++ device/python/start.sh
@@ -1,9 +1,9 @@
-sudo su
-
-cd /opt/SmartHouseSystem/device/python
-
-source venv/bin/activate
+source /opt/SmartHouseSystem/device/python/venv/bin/activate
 
 export PYTHONPATH="/opt/SmartHouseSystem"
 
-python3 device/python/main.py
+python3 /opt/SmartHouseSystem/device/python/main.py arg1 arg2
+if [ $? -ne 0 ]; then
+    echo "Python script failed!"
+    exit 1
+fi
